6c46e16cab2813d16302814fe457077089d3cd20
Paweł Adamski <pawel.adamski@inspeerity.com>
2024-08-06 00:43:01 +0200

    v2

--- Program.cs
+++ Program.cs
@@ -4,48 +4,46 @@ using System.Collections.Concurrent;
 var owner = "lodash";
 var repo = "lodash";
 var sha = "main";
+var token = "";
 var lettersRange = Enumerable.Range('a', 26);
+var maxDegreeOfParallelism = 10;
 
-// Get content
+// Get GitHubClient
 var client = new GitHubClient(new ProductHeaderValue(System.Diagnostics.Process.GetCurrentProcess().ProcessName));
+
+if (!string.IsNullOrWhiteSpace(token))
+{
+    client.Credentials = new Credentials(token);
+}
+
+// Get files
 var treeResponse = await client.Git.Tree.GetRecursive(owner, repo, sha);
 var files = treeResponse.Tree.Where(item => item.Path.EndsWith(".js") || item.Path.EndsWith(".ts")).ToList();
 
 // Count letters frequency
 var letterFrequencies = new ConcurrentDictionary<char, int>();
 
-foreach (var item in lettersRange)
-{
-    letterFrequencies[(char)item] = 0;
-}
-
-var tasks = files.Select(async file =>
+await Parallel.ForEachAsync(files, new ParallelOptions { MaxDegreeOfParallelism = maxDegreeOfParallelism }, async (file, _) =>
 {
     var fileContents = await client.Repository.Content.GetAllContentsByRef(owner, repo, file.Path, sha);
+    var letters = fileContents[0].Content.ToLower();
 
-    var content = fileContents[0].Content.ToLower();
-
-    foreach (var letter in content)
+    foreach (var letter in letters)
     {
-        if (!char.IsLetter(letter))
+        if (lettersRange.Contains(letter))
         {
-            continue;
+            letterFrequencies.AddOrUpdate(letter, 1, (_, oldValue) => Interlocked.Increment(ref oldValue));
         }
-
-        if (letterFrequencies.ContainsKey(letter))
-        {
-            letterFrequencies[letter]++;
-        }
-    }
+    };
 });
 
- await Task.WhenAll(tasks);
-
 // Sort by frequency in decreasing order
-var sortedFrequencies = letterFrequencies.OrderByDescending(kv => kv.Value);
+var sortedFrequencies = letterFrequencies.OrderByDescending(x => x.Value);
 
 // Display results
+Console.WriteLine($"Letters count: {sortedFrequencies.Sum(x => x.Value)}");
 Console.WriteLine($"Letter Frequencies in {owner}/{repo} repository (JavaScript/TypeScript Files):");
+
 foreach (var (letter, frequency) in sortedFrequencies)
 {
     Console.WriteLine($"{letter}: {frequency}");
